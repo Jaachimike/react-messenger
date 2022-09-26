e99714592db73481d5cb38754b12f1464240280e
Jaachimike <okaforjaachi@gmail.com>
2022-09-26 03:36:12 +0100

    Stopped at adding Private Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import AuthProvider from './context/auth'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
@@ -15,7 +16,9 @@ function App() {
         <Routes>
           <Route exact path='/register' element={<Register/>} />
           <Route exact path='/login' element={<Login/>} />
-          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/' element={<PrivateRoute/>}>
+            <Route exact path='/' element={<Home/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
--- src/context/auth.js
+++ src/context/auth.js
@@ -1,6 +1,7 @@
 import {createContext, useEffect, useState} from 'react'
 import {onAuthStateChanged} from 'firebase/auth'
 import {auth} from '../firebase'
+import Loading from '../components/Loading'
 
 export const AuthContext = createContext()
 
@@ -16,7 +17,7 @@ const AuthProvider = ({children}) => {
       })
   }, []);
   if (loading) {
-    return "loading";
+    return <Loading/>;
   }
   return <AuthContext.Provider value={{user}}>{children}</AuthContext.Provider>
 }
